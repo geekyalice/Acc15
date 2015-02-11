19e0c0733a5195ee64d361ea59834009bad9cb36
garima_mahajan <garima_mahajan@B6D1TZK1S.techlabs.accenture.com>
2015-02-11 15:40:01 +0530

    added a print message

--- test2/src/test2/test2.java
+++ test2/src/test2/test2.java
@@ -1,13 +1,11 @@
 package test2;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.util.Scanner;
-
 public class test2 {
 
 	public static void main(String[] args) {
 		System.out.println("Why this kolavari di?");
+		
+		System.out.println("now    finalllyyy");
 	}
 
 }
